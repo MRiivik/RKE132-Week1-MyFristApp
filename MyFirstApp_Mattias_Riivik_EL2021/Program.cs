174af67bb4875267193aa4461af4a9d2be8a296b
MRiivik <mriivik@tktk.ee>
2023-01-26 18:06:27 +0200

    create new app

---
+++ MyFirstApp_Mattias_Riivik_EL2021/Program.cs
@@ -0,0 +1,15 @@
+﻿// This is a comment to my code
+
+// Rakendus küsib kasutajal sisestada oma nime
+// Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Please enter your name:"); //Output
+
+String userName = Console.ReadLine(); //Input
+// "=" - salvesta väärtus
+// String - sõne
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+// String interpolation
+
+Console.WriteLine($"Hello, {userName} !"); //Output
